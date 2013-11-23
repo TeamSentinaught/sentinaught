d367891a923f8188cf4290a69f076fc76e23daa5
iainjmitchell <iainjmitchell@gmail.com>
2013-11-23 12:35:44 +0000

    Test monitor is informed of failure when unsuccesful test run

--- test/mocha-test-runner-tests/run-mocha-tests.js
+++ test/mocha-test-runner-tests/run-mocha-tests.js
@@ -2,12 +2,26 @@ var MochaTestRunner = require('../../lib/mocha-test-runner');
 require('chai').should();
 
 test('Mocha test run passes when standard ui run against all-okay-standard-ui', function(done){
-	var	testMonitor = {
+	var	mockTestMonitor = {
 			passed : done,
 		},
 		testOptions = {
-			tests : ['dummy-tests/all-okay-standard-ui.js']
+			tests : ['test/mocha-test-runner-tests/dummy-tests/all-okay-standard-ui.js']
 		}
-		mochaTestRunner = new MochaTestRunner(testOptions, testMonitor);
+		mochaTestRunner = new MochaTestRunner(testOptions, mockTestMonitor);
+	mochaTestRunner.start();
+});
+
+test('Mocha test run fails when standard ui run against all-bad-standard-ui', function(done){
+	var	mockTestMonitor = {
+			passed : function(){
+				done('Expected test runner failure');
+			},
+			failed : done
+		},
+		testOptions = {
+			tests : ['test/mocha-test-runner-tests/dummy-tests/all-bad-standard-ui.js']
+		}
+		mochaTestRunner = new MochaTestRunner(testOptions, mockTestMonitor);
 	mochaTestRunner.start();
 });
