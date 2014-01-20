b14589ca8994b78d907e06575760e7e320f31a9d
iainjmitchell <iainjmitchell@gmail.com>
2014-01-20 08:48:08 +0000

    no commit message

--- gulpfile.js
+++ gulpfile.js
@@ -15,25 +15,29 @@ gulp.task('mocha', function () {
 });
 
 gulp.task('jshint', function(){
+	var eventStream = require('event-stream');
+	var failed = false;
+	var failWatcher = eventStream.map(function (file, cb) {
+		if (!file.jshint.success) return cb(new Error("JSHint failed"), file);
+		cb(null, file);
+	});
 	gulp
 		.src(['lib/**/*.js', 'test/**/*.js'])
 		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(failWatcher);
 });
 
-gulp.task('test', function(){
-	gulp.run('jshint', 'mocha');
-});
+gulp.task('test', ['jshint', 'mocha']);
 
-gulp.task('push', function(){
+gulp.task('push', function(e){
 	var commitMessage = gulp.env.message || 'no commit message';
 	gulp
 		.src('./.')
 		.pipe(git.add())
 		.pipe(git.commit(commitMessage))
 		.pipe(git.push());
+	console.log('push');
 });
 
-gulp.task('default', function(){
-	gulp.run('test', 'push');
-});
+gulp.task('default', ['test', 'push']);
